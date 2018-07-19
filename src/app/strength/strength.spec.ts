1d575db060ee4a91c9518a6b11c9a31840a50f91
Sen M <sentvla@gmail.com>
2018-07-19 13:28:52 -0700

    before each added to init p

--- src/app/strength/strength.spec.ts
+++ src/app/strength/strength.spec.ts
@@ -1,18 +1,21 @@
 import { StrengthPipe } from './strength.pipe';
 
 describe('StrengthPipe', ()=>{
+
+    let p: StrengthPipe;
+
+    beforeEach(()=>{
+        p = new StrengthPipe();
+    })
     it('should return weak if 5 is passed', ()=>{
-        let p = new StrengthPipe();
         expect(p.transform(5)).toEqual('5 (weak)');
     });
 
     it('should return strong if value between 10 and 20', ()=> {
-        let p = new StrengthPipe();
         expect(p.transform(12)).toEqual('12 (strong)');
     });
 
     it('should return unbelievable if value greater than 20', ()=>{
-        let p = new StrengthPipe();
         expect(p.transform(20)).toEqual('20 (unbelievable)');
     })
 });
