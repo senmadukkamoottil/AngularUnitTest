80ce588198abf41336be6bc39973376f9ad022cf
Sen M <sentvla@gmail.com>
2018-07-20 18:40:51 -0700

    faked child component insted of NO_ERRORS_SCHEMA

--- src/app/heroes/heroes.component.shallow.spec.ts
+++ src/app/heroes/heroes.component.shallow.spec.ts
@@ -1,11 +1,22 @@
 import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { HeroesComponent } from './heroes.component';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { NO_ERRORS_SCHEMA, Input, Component } from '@angular/core';
 import { HeroService } from '../hero.service';
 import { of } from 'rxjs/observable/of';
+import { Hero } from '../hero';
 
 describe('HerosComponent', () => {
+    
     let fixture: ComponentFixture<HeroesComponent>;
+
+    //moking the child component HeroComponent
+    @Component({
+        selector: 'app-hero',
+        template: '<div></div>',
+      })
+      class FakeHeroComponent {
+        @Input() hero: Hero;
+      }
     
     //moking injected service
     let mokHeroService = jasmine.createSpyObj(['getHeroes', 'deleteHero', 'updateHero']);
@@ -16,16 +27,18 @@ describe('HerosComponent', () => {
     ]
     beforeEach( () => {
         TestBed.configureTestingModule({
-            declarations: [HeroesComponent],
+            declarations: [
+                HeroesComponent,
+                FakeHeroComponent
+            ],
             // the injected service
             providers: [
             { provide: HeroService, useValue: mokHeroService }
             ],
-            schemas: [NO_ERRORS_SCHEMA]
+            // schemas: [NO_ERRORS_SCHEMA]
         });
         fixture = TestBed.createComponent(HeroesComponent);
     });
-    
     
      it('should return heroes from the HeroService', () => {
          mokHeroService.getHeroes.and.returnValue(of(HEROES));
